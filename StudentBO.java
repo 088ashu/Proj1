1398512039ff45f32117848010a646d6a2ee9c05
088ashu <53143203+088ashu@users.noreply.github.com>
2019-07-21 17:33:44 +0530

    added new proj1

---
+++ StudentBO.java
@@ -0,0 +1,35 @@
+package com.nt.bo;
+
+public final class StudentBO {
+	
+	private String s_name;
+	private int total;
+	private float percentage;
+	private String result;
+	
+	public String getS_name() {
+		return s_name;
+	}
+	public void setS_name(String s_name) {
+		this.s_name = s_name;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	public float getPercentage() {
+		return percentage;
+	}
+	public void setPercentage(float percentage) {
+		this.percentage = percentage;
+	}
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+}
